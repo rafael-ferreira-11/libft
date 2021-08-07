77076d5ff67f673a24fe426534a353c57bc6c7c6
Rafael Ferreira <ferreira@asia.com>
2021-08-07 16:44:14 -0300

    a

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,8 +6,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		index;
 	int		index_s2;
 
+	if (!s1 || !s2)
+		return (0);
 	res = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (!res || !s1 || !s2)
+	if (!res)
 		return (0);
 	index = 0;
 	while (s1[index])
