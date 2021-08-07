2b8b60907210c04ccc31665d31fe1634ff8848e5
Rafael Ferreira <ferreira@asia.com>
2021-08-07 17:21:41 -0300

    f

--- ft_split.c
+++ ft_split.c
@@ -1,62 +1,69 @@
 #include "libft.h"
 
-static int	count_words(const char *str, char c)
+static	int	ft_count(const char *str, char c)
 {
-	int i;
-	int trigger;
+	int	i;
+	int	count;
 
 	i = 0;
-	trigger = 0;
-	while (*str)
+	count = 1;
+	while (str[i])
 	{
-		if (*str != c && trigger == 0)
+		if (str[i] == c)
 		{
-			trigger = 1;
+			i += 2;
+			count++;
+		}
+		else
 			i++;
-		}
-		else if (*str == c)
-			trigger = 0;
-		str++;
 	}
-	return (i);
+	return (count);
 }
 
-static char	*word_dup(const char *str, int start, int finish)
+static char	*ft_copy(const char *str, int start, int finish)
 {
-	char	*word;
-	int		i;
-
-	i = 0;
-	word = malloc((finish - start) * sizeof(char));
-	while (start < finish)
-		word[i++] = str[start++];
-	word[i] = '\0';
-	return (word);
-}
-
-char		**ft_split(char const *s, char c)
-{
-	size_t	i;
-	size_t	j;
+	char	*ret;
 	int		index;
-	char	**split;
 
-	if (!s || !(split = malloc((count_words(s, c) + 1) * sizeof(char *))))
+	index = 0;
+	ret = malloc((finish - start + 1));
+	if (!ret)
 		return (0);
-	i = 0;
-	j = 0;
-	index = -1;
-	while (i <= ft_strlen(s))
+	while (start < finish)
 	{
-		if (s[i] != c && index < 0)
-			index = i;
-		else if ((s[i] == c || i == ft_strlen(s)) && index >= 0)
-		{
-			split[j++] = word_dup(s, index, i);
-			index = -1;
-		}
-		i++;
+		ret[index] = str[start];
+		index++;
+		start++;
 	}
-	split[j] = 0;
-	return (split);
+	ret[index] = '\0';
+	return (ret);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		start;
+	size_t	index;
+	size_t	items;
+	char	**ret;
+
+	ret = malloc((ft_count(s, c) + 1) * sizeof(char *));
+	if (!s || !ret)
+		return (0);
+	index = 0;
+	items = 0;
+	start = -1;
+	while (index <= ft_strlen(s))
+	{
+		if (s[index] != c && start < 0)
+			start = index;
+		else if ((s[index] == c || index == ft_strlen(s)) && start >= 0)
+		{
+			ret[items] = ft_copy(s, start, index);
+			items++;
+			start = -1;
+		}
+		index++;
+	}
+	ret[items] = 0;
+	return (ret);
 }
