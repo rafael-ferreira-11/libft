54d20654aaa4377cfc8d2f8c9d169f26b1b8ee5e
Rafael Ferreira <ferreira@asia.com>
2021-08-07 20:18:48 -0300

    f

--- ft_substr.c
+++ ft_substr.c
@@ -2,20 +2,16 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*res;
-	size_t	index;
+	char	*new_str;
+	size_t	i;
+	size_t	j;
 
-	res = (char *)malloc(len + 1);
-	if (!res || !s)
+	if (!s || !(new_str = (char *)malloc(len + 1)))
 		return (0);
-	index = 0;
-	while (index < len && start + index < ft_strlen(s))
-	{
-		if (!s[start + index])
-			break ;
-		res[index] = s[start + index];
-		index++;
-	}
-	res[index] = 0;
-	return (res);
+	i = start;
+	j = 0;
+	while (i < ft_strlen(s) && j < len)
+		new_str[j++] = s[i++];
+	new_str[j] = '\0';
+	return (new_str);
 }
