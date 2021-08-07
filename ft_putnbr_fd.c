1f523b4a52ff2af2702432a60cfb35f86ff62029
Rafael Ferreira <ferreira@asia.com>
2021-08-07 18:10:25 -0300

    f

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -2,8 +2,18 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*str;
+	unsigned int	numero;
 
-	str = ft_itoa(n);
-	ft_putstr_fd(str, fd);
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		numero = (unsigned int)(n * -1);
+	}
+	else
+		numero = (unsigned int)n;
+	if (numero >= 10)
+	{
+		ft_putnbr_fd((numero / 10), fd);
+	}
+	ft_putchar_fd((numero % 10), fd);
 }
