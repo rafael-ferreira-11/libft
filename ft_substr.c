fa7f1ad7d7849a481835b76096b811a995911f19
Rafael Ferreira <ferreira@asia.com>
2021-08-07 21:18:06 -0300

    d

--- ft_substr.c
+++ ft_substr.c
@@ -9,7 +9,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (0);
 	if ((size_t)start > ft_strlen(s))
 		return (ft_strdup(""));
-	res = (char *)malloc(len + 1);
+	if (ft_strlen(s) - start > len)
+		res = (char *)malloc(len + 1);
+	else
+		res = (char *)malloc(ft_strlen(s) - start + 1);
 	if (!res)
 		return (0);
 	index = 0;
